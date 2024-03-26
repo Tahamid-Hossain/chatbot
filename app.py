b9ba9fdee464f9f93c14885110e284900b692e75
tahamid <mitahamid13@gmail.com>
2024-03-26 04:54:59 +0400

    new code visin bot added

--- app.py
+++ app.py
@@ -79,6 +79,24 @@ def get_gemini_response(input,image):
     return response.text
 
 
+def chat_func(prompt, img):
+    model = genai.GenerativeModel('gemini-pro-vision')
+
+    try:
+        response = model.generate_content([prompt, img])
+        return response.text
+    except Exception as e:
+        return f"An error occured: {e}. Please try again."
+
+
+def convert_image_to_pil(st_image):
+    import io
+    from PIL import Image
+    image_data = st_image.read()
+    pil_image = Image.open(io.BytesIO(image_data))
+    return pil_image
+
+
 def main():
     set_page_config()
     st.title("🤖 Gemini Bot 🤖")
@@ -178,12 +196,29 @@ def main():
             st.markdown(html_content, unsafe_allow_html=True)
 
     if selected == "Vision Bot":
-        input=st.text_input("Input Prompt: ",key="input")
-        uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
-        image=""   
-        if uploaded_file is not None:
-            image = Image.open(uploaded_file)
-            st.image(image, caption="Uploaded Image.", use_column_width=True)
+        img = st.file_uploader("Upload an image", type=['png', 'jpg', 'jpeg','gif'])
+
+        if img:
+            st.image(img, caption='Chat with this image')
+
+            prompt = st.text_area('Ask a question about your image:')
+
+            if prompt:
+                pil_image = convert_image_to_pil(img)
+
+                with st.spinner('Analyzing your image ...'):
+                    answer = chat_func(prompt, pil_image)
+                    st.text_area('Gemini Answer: ', value=answer)
+
+
+                if 'history' not in st.session_state:
+                    st.session_state.history = 'Chat History\n'
+
+                value = f'**Question**: {prompt}: \n\n **Answer**: {answer}'
+                st.session_state.history = f'{value} \n\n {"-" * 100} \n\n {st.session_state.history}'
+
+                # h = st.session_state.history
+                # st.text_area(label='Chat History:', value=h, height=800, key='history')
 
 if __name__ == "__main__":
     main()
